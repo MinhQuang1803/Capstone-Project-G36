5a968ba34fe7b738b8e0d57ed4f307c13f7e10e8
DamNhat <mikuotngokm@gmail.com>
2022-06-26 16:30:50 +0700

    create product detail

---
+++ Code/back-end/banhangma-be/banhangma-be/Models/Carts.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace banhangma_be.Models
+{
+    public partial class Carts
+    {
+        public Carts()
+        {
+            CartNote = new HashSet<CartNote>();
+        }
+
+        public int Id { get; set; }
+        public int? Userid { get; set; }
+        public bool IsDeleted { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public int? CreatedBy { get; set; }
+        public DateTime? UpdatedDate { get; set; }
+        public int? UpdatedBy { get; set; }
+        public int? ProductId { get; set; }
+        public int? PriceId { get; set; }
+        public int Quantity { get; set; }
+
+        public virtual ProductPrice Price { get; set; }
+        public virtual Products Product { get; set; }
+        public virtual Users User { get; set; }
+        public virtual ICollection<CartNote> CartNote { get; set; }
+    }
+}
---
+++ Code/back-end/banhangma-be/banhangma-be/Models/ProductSize.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace banhangma_be.Models
+{
+    public partial class ProductSize
+    {
+        public ProductSize()
+        {
+            ProductPrice = new HashSet<ProductPrice>();
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public bool IsDeleted { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public int? CreatedBy { get; set; }
+        public DateTime? UpdatedDate { get; set; }
+        public int? UpdatedBy { get; set; }
+
+        public virtual ICollection<ProductPrice> ProductPrice { get; set; }
+    }
+}
