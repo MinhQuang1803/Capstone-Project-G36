abe480d691327be77ccf51ba3e3dd3fcc2a97fc5
flexer18 <97142476+flexer18@users.noreply.github.com>
2022-06-28 07:43:10 +0700

    create order detail note
    create new order detail note

---
+++ code/back-end/banhangma-be/banhangma-be/Models/OrderDetailNote.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace banhangma_be.Models
+{
+    public partial class OrderDetailNote
+    {
+        public int Id { get; set; }
+        public int? OrderDetailId { get; set; }
+        public string Note { get; set; }
+        public int Price { get; set; }
+        public bool IsDeleted { get; set; }
+
+        public virtual OrderDetail OrderDetail { get; set; }
+    }
+}
