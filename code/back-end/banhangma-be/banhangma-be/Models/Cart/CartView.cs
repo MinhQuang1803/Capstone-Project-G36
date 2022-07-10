198e8d3e2fea3be4c8608f4c641215398c8398f2
tuanba16 <btuan8077@gmail.com>
2022-07-10 17:43:19 +0700

    update cart

---
+++ code/back-end/banhangma-be/banhangma-be/Models/Cart/CartView.cs
@@ -0,0 +1,20 @@
+﻿namespace banhangma_be.Models.Cart
+{
+    public class CartView
+    {
+        public int Id { get; set; }
+        public int? Userid { get; set; }
+        public int? ProductId { get; set; }
+        public int Quantity { get; set; }
+        public int? Colorid { get; set; }
+        public bool Ispay { get; set; }
+        public int? SizeId { get; set; }
+        public string NameSize { get; set; }
+        public string NameColor { get; set; }
+        public string NameProduct { get; set; }
+        public string Images { get; set; }
+        public int PriceProduct { get; set; }
+
+        public int? Companyid { get; set; }
+    }
+}
